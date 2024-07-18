569d8b5554f7b8368b8e865694a6d4afacaa6e1d
Lim Sungmin <88964580+sungminstar@users.noreply.github.com>
2024-07-18 23:09:39 +0900

    [level 2] Title: 올바른 괄호, Time: 4.98 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s){
+    let stack = [];
+    for(let i = 0; i < s.length; i++){
+        if (s[i] === '(') {
+            stack.push('(');
+        }
+        if (s[i] === ')') {
+            if (stack.length > 0 && stack[stack.length - 1] === '(') {
+                stack.pop();}
+            else return false;
+           
+        }
+    }
+    if (stack.length === 0) return true;
+    return false;
+}
