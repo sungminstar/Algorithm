aa7e7d127ecabe14f83dfe30c6758b3dc5745643
Lim Sungmin <88964580+sungminstar@users.noreply.github.com>
2024-07-21 15:54:49 +0900

    [Silver IV] Title: 로프, Time: 292 ms, Memory: 24392 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.js
+++ 백준/Silver/2217. 로프/로프.js
@@ -1,23 +1,16 @@
- var input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+var input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 //var input = require("fs").readFileSync("example.txt").toString().split("\n");
 
-// 입력 데이터 처리
 let inputToNum = input.map((x) => parseInt(x));
-let n = inputToNum[0]; // 첫 번째 요소는 로프의 개수
-let lopeInfo = inputToNum.slice(1); // 나머지는 로프의 중량 정보
-
-// 로프 중량 정보를 내림차순으로 정렬
-lopeInfo.sort((a, b) => b - a);
+n = inputToNum[0];
+let lopeInfo = inputToNum.slice(1);
+lopeInfo = lopeInfo.sort((a, b) => b - a);
 
 let maxWeight = 0;
 
-// 로프 개수를 하나씩 늘려가며 최대 중량 계산
 for (let i = 0; i < n; i++) {
-  // i개의 로프를 사용할 때 들어올릴 수 있는 최대 중량
   let weight = lopeInfo[i] * (i + 1);
-  if (weight > maxWeight) {
-    maxWeight = weight;
-  }
+  if (weight > maxWeight) maxWeight = weight;
 }
 
 console.log(maxWeight);
