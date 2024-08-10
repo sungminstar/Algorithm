f518ea22a61ece75a8cadb87a05dcb6735436f66
Lim Sungmin <88964580+sungminstar@users.noreply.github.com>
2024-08-11 07:23:40 +0900

    [level 2] Title: 구명보트, Time: 29.02 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    let answer = 0;
+    let i = 0; 
+    let j = people.length - 1;
+
+    people.sort((a, b) => a - b);
+
+    while (i <= j) {
+        if (people[i] + people[j] <= limit) {
+            i++;  
+        }
+        j--;  
+        answer++;  
+    }
+
+    return answer;
+}
