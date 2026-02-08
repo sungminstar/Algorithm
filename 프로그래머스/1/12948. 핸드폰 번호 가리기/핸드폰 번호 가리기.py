4d102c5de7cdf2fc7ae3671867c55a8ebb552b1f
Lim Sungmin <88964580+sungminstar@users.noreply.github.com>
2026-02-08 18:07:16 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -0,0 +1,9 @@
+def solution(phone_number):
+    answer = ''
+    
+    
+    for i in range(0, len(phone_number) - 4) :
+        answer += '*'
+    answer += phone_number[-4:]
+    
+    return answer
