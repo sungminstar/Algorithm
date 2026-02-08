db4953a95ebe652137ac37a3ed51535e42e353ed
Lim Sungmin <88964580+sungminstar@users.noreply.github.com>
2026-02-08 17:56:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,9 @@
+def solution(num):
+    answer = 0
+    while num != 1 and answer < 500:
+        num = num // 2 if num % 2 == 0 else num * 3 + 1
+        answer += 1
+    if answer == 500 :
+        answer = -1
+        
+    return answer
