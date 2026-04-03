c300487fe5410ce5cb73be38f7efca8b7f1fd0d4
Lim Sungmin <88964580+sungminstar@users.noreply.github.com>
2026-04-03 14:29:55 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,7 @@
 def solution(array, commands):
     answer = []
     for i in range(len(commands)) :
-        temp = sorted(array[commands[i][0]-1:commands[i][1]]);
-        idx = commands[i][2]-1;
-        answer.append(temp[idx])
+        tmp_arr = array[commands[i][0]-1 : commands[i][1]]
+        sort_arr = sorted(tmp_arr)
+        answer.append(sort_arr[commands[i][2] - 1])        
     return answer
