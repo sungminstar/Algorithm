7776fd135085c0feedc6ed19edfc237c187e3afc
Lim Sungmin <88964580+sungminstar@users.noreply.github.com>
2026-04-03 15:19:39 +0900

    [level 2] Title: 가장 큰 수, Time: 1300.61 ms, Memory: 26 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,4 @@
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)  
-    result = ''.join(numbers)
-    return result if result[0] != "0" else "0"
+def solution(numbers) :
+    numbers = list(map(str, numbers)) 
+    numbers.sort(key=lambda x : x * 3, reverse=True)
+    return str(int(''.join(numbers)))
