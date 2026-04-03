317bc187daac69b63cc781dc275b52d067f8cc45
Lim Sungmin <88964580+sungminstar@users.noreply.github.com>
2026-04-03 15:44:32 +0900

    [level 2] Title: H-Index, Time: 318.20 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,10 @@
-def solution(citations):
-    citations.sort(reverse=True)
-    for idx, citation in enumerate(citations) :
-        if idx + 1 > citation :
-            return idx
-    return len(citations)
+def solution(citations) :
+    for i in range(max(citations) , -1, -1) :
+        c = 0
+        for j in citations :
+            if j >= i : 
+                c += 1
+        if c >= i :
+            return i
+    return 0
+        
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.14 ms
+메모리: 9.11 MB, 시간: 318.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 07일 22:00:01
+2026년 04월 03일 15:44:28
 
 ### 문제 설명
 
